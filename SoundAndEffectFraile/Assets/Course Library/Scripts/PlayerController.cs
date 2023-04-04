11dcad86d7b87109bbba25a7281278757090a63f
jeofrai1e <jeorocks323@gmail.com>
2023-04-04 11:42:13 -0700

    klkl
    lk

--- SoundAndEffectFraile/Assets/Course Library/Scripts/PlayerController.cs
+++ SoundAndEffectFraile/Assets/Course Library/Scripts/PlayerController.cs
@@ -31,7 +31,7 @@ public class PlayerController : MonoBehaviour
         }
 
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision )
     {
         if (collision.gameObject.CompareTag("Ground"))
       
